5aef309b3ea553aea869dd9f5f21c051e79e6578
Lorenz Meier <lorenz@px4.io>
2016-08-27 17:27:59 +0200

    MAVLink: We can work with less stack

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -2373,7 +2373,7 @@ Mavlink::start(int argc, char *argv[])
 	px4_task_spawn_cmd(buf,
 			   SCHED_DEFAULT,
 			   SCHED_PRIORITY_DEFAULT,
-			   2400,
+			   2200,
 			   (px4_main_t)&Mavlink::start_helper,
 			   (char *const *)argv);
 
